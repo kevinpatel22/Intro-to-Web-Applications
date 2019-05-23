cda11979615f5dc3410593d7569f16a1742a7f15
Kevin <mail.kevinpatel@gmail.com>
2019-05-23 16:17:55 -0400

    created about_me url

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -24,13 +24,21 @@ def home_page(request):
     return HttpResponse(response)
 
 def portfolio_page(request):
-    random_number = randint(0, 100)
-    image_url = "https://picsum.photos/800/1000/?image={}".format(random_number)
-    context = {'gallery_image': image_url}
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0, 100)
+        image_urls.append("https://picsum.photos/800/1000/?image={}".format(random_number))
+    context = {'gallery_images': image_urls}
     response = render(request, 'gallery.html', context)
+    return HttpResponse(response)
+
+def about_me_page(request):
+    context = {'skills': ['Python', 'HTML', 'CSS','Django'], 'interests': ['Business', 'Travel']}
+    response = render(request,'about.html', context)
     return HttpResponse(response)
 
 urlpatterns = [
     path('home/', home_page),
-    path('portfolio/', portfolio_page)
+    path('portfolio/', portfolio_page),
+    path('about_me/', about_me_page)
 ]
